b2b215bd7a908a2c35c0af416b1f26f0a495ca89
Qematy <meldisoukyy@gmail.com>
2023-05-09 19:26:24 +0000

    chore: add server.albdahaqar.com to allowed hosts

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tx7u=lt-v2l*pi%iwy@*gf^8#n0sy3gt=r^=q9d&3%bod9co(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hazem179.pythonanywhere.com', '127.0.0.1', 'albdahaqar.com', '164.90.220.106']
+ALLOWED_HOSTS = ['hazem179.pythonanywhere.com', '127.0.0.1', 'albdahaqar.com', '164.90.220.106', 'server.albdahaqar.com']
 
 # Application definition
 
@@ -154,3 +154,4 @@ AUTH_USER_MODEL = 'accounts.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
