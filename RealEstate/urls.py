b82feead2315a70c9e5bbe58a26e160c7072e346
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-05-10 12:44:43 +0300

    feat: add homepage

--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/', include('api.urls')),
