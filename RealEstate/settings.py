c93d46600c1abe3fcf0d466b473f0b33c43f7b35
Hazem <hazemm913@gmail.com>
2023-05-16 14:54:12 +0300

    sqlite settings

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -96,14 +96,20 @@ WSGI_APPLICATION = 'RealEstate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'albdah',
+#         'USER': 'qematy',
+#         'PASSWORD': 'qematy_password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'albdah',
-        'USER': 'qematy',
-        'PASSWORD': 'qematy_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
